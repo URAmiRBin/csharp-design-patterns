05d429cc42592643478a21f1f994477f765b7061
Amir Bin <ur.amirbin@gmail.com>
2021-03-25 10:56:24 +0430

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace csharp_design_patterns
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
